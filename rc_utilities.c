83495bda0bcaef2405b243db04cf6d88991832fc
Robert Clarke <clarke.john.robert@gmail.com>
2021-11-30 12:52:21 -0500

    Added missing include

--- rc_utilities.c
+++ rc_utilities.c
@@ -1,6 +1,7 @@
 #include "rc_utilities.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <stdarg.h> //For variadic functions
 
 //Note: prefix is optional, pass NULL to ignore
